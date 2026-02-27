2545fb382392e6efd2199afb71dbb85a17d41afa
Divyasasi24 <divyasasi24@gmail.com>
2026-02-27 22:43:04 +0530

    add check

--- stg1.py
+++ stg1.py
@@ -9,7 +9,7 @@ if operator == '+':
 elif operator == '-':
     result = num1 - num2
     print("Result =", result)
-
+                
 elif operator == '*':
     result = num1 * num2
     print("Result =", result)
